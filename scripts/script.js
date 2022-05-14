35bc434fbc70064786ad0076ef43d940f41dca80
Max Bobrov <maxbobrov1997@gmail.com>
2022-05-14 13:28:15 +0300

    started jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,26 @@
 
 $(document).ready(function () {
-    
 
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000, 'linear');
+    });
+
+    const btnUp = $('.btnUp')
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 300){
+            btnUp.fadeIn();
+        }else{
+            btnUp.fadeOut();
+        }
+    });
+    btnUp.on('click', function(){
+        $('html, body').animate({
+        scrollTop: 0
+        }, 1000, 'linear');
+        return false;
+    });
 });
